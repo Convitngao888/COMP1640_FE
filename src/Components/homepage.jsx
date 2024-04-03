6a26ac103a6a19081061d5c727a8dd5764a69a84
LamPhucThinh <thinhlpgcs210915@fpt.edu.vn>
2024-04-03 22:46:28 +0700

    update

--- src/Components/homepage.jsx
+++ src/Components/homepage.jsx
@@ -1,13 +1,19 @@
 import './homepage.css'
-import Header from './header';
-import Footer from './footer';
 import { useNavigate, Navigate } from 'react-router-dom';
 import { useAuth } from './AuthContext';
+import { Layout, Menu, theme } from 'antd';
+const { Header, Content, Footer } = Layout;
+const items = new Array(15).fill(null).map((_, index) => ({
+    key: index + 1,
+    label: `nav ${index + 1}`,
+  }));
 
 const Homepage = () => {
     const navigate = useNavigate();
     const { accessToken, logout } = useAuth();
-
+    const {
+        token: { colorBgContainer, borderRadiusLG },
+      } = theme.useToken();
     const handleLogout = () => {
         logout();
         navigate('/');
@@ -23,12 +29,52 @@ const Homepage = () => {
         accessToken ? (
             
             <div className='home-container'>
-                <Header/>
-                <h1>Home Page</h1>
-                <button onClick={handleLogout}>Log out</button> 
-                <button onClick={handleAccessStudent}>student</button>
-                <button onClick={handleAccessAdmin}>admin</button>
-                <Footer/>
+                <Layout>
+                    <Header
+                        style={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        }}
+                    >
+                        <div className="demo-logo" />
+                        <Menu
+                        theme="dark"
+                        mode="horizontal"
+                        defaultSelectedKeys={['2']}
+                        items={items}
+                        style={{
+                            flex: 1,
+                            minWidth: 0,
+                        }}
+                        />
+                    </Header>
+                    <Content
+                        style={{
+                        padding: '0 48px',
+                        }}
+                    >
+                        <div
+                        style={{
+                            background: colorBgContainer,
+                            minHeight: 280,
+                            padding: 24,
+                            borderRadius: borderRadiusLG,
+                        }}
+                        >
+                        <button onClick={handleLogout}>Log out</button> 
+                        <button onClick={handleAccessStudent}>student</button>
+                        <button onClick={handleAccessAdmin}>admin</button>
+                        </div>
+                    </Content>
+                    
+                    <Footer
+                        style={{
+                        textAlign: 'center',
+                        }}
+                    >
+                        Alamak ©{new Date().getFullYear()} Created by Th0nk
+                    </Footer>
+                </Layout>
             </div>
         ) : (
             <Navigate to="/" />
